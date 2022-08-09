ac36707b4675d74a75f9d9b56671a6d68add8695
Brennan <brennan.davis@outlook.com>
2022-08-08 22:14:09 -0400

    initial commit

---
+++ Pet.cs
@@ -0,0 +1,11 @@
+﻿namespace GitBranchingGroup2ElectricBoogaloo
+{
+    public class Pet
+    {
+        public string Name { get; set; }
+        public virtual void MakeHappyNoise()
+        {
+            Console.WriteLine("caw caw!");
+        }
+    }
+}
