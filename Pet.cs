5579ff5d0f8e762c30a4229f955aa3a1cc5ffbf5
morgan <pttrsn.mrgn@gmail.com>
2022-08-08 21:37:29 -0500

    added dog class and color property

--- Pet.cs
+++ Pet.cs
@@ -5,6 +5,8 @@
         public string Name { get; set; }
 
         public int NumOfWhiskers { get; set; }
+        public string Color { get; set; }
+
         public virtual void MakeHappyNoise()
         {
             Console.WriteLine("caw caw!");
